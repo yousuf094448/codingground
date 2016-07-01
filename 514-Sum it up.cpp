3a7dedca7d0875c7a188f47c5df7becfacd846a4
yousuf094448 <faishal.yousuf@yahoo.com>
2016-07-01 17:02:12 +0600

    Update 514-Sum it up.cpp

--- 514-Sum it up.cpp
+++ 514-Sum it up.cpp
@@ -3,17 +3,50 @@ using namespace std;
 
 int input[15];
 int t,n;
+int temp[15]={0};
 
 void recursion(int level, int sum, int ans[], int index){
     if(sum == t){
-        int flag=false;
-        for(int i = 0; i<=index; i++){
+        int flag=0;
+        int tem[15];
+        // for( i = 0; i<=n; i++){
+        //     cout<<temp[i]<<" "<<ans[i]<<endl;
+        //     if(temp[i]!=ans[i]) break;
+        // }
+        // if(i==n) return;
+        int j=0;
+        for(int i = 0; i<=n; i++){
+            //temp[i]=ans[i];
             if(ans[i]){
-                if(flag)cout<<"+";
-                cout<<ans[i];
-                flag=true;
+                // if(flag)cout<<"+";
+                tem[j++]=ans[i];
+                // flag=true;
             } 
         }
+        for(int i = 0; i<j; i++){
+            if(temp[i] != tem[i]) {
+                flag = 1;
+                break;
+            }
+        }
+        if(!flag) return;
+        // j=0;
+        flag=0;
+        for(int i = 0; i<j; i++){
+            if(i)cout<<"+";
+                cout<<tem[i];
+                temp[i]=tem[i];
+                // flag=1;
+        }
+        // for(int i = 0; i<=n; i++){
+        //   // temp[i]=ans[i];
+        //     if(ans[i]){
+        //         if(flag)cout<<"+";
+        //         cout<<ans[i];
+        //         temp[j++]=ans[i];
+        //         flag=1;
+        //     } 
+        // }
         cout<<endl;
         return;
     }
