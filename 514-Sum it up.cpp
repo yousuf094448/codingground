4cc1ff8484a82761bc569c9bae624776ed54efd1
yousuf094448 <faishal.yousuf@yahoo.com>
2016-07-01 14:44:12 +0600

    Update 514-Sum it up.cpp

--- 514-Sum it up.cpp
+++ 514-Sum it up.cpp
@@ -4,15 +4,23 @@ using namespace std;
 int input[15];
 int t,n;
 
-void recursion(int level, int sum){
+void recursion(int level, int sum, int ans[], int index){
     if(sum == t){
-        cout << "yes" << endl;
+        for(int i = 0; i<=index; i++){
+            if(ans[i]) cout<<ans[i]<<"+";
+        }
+        cout<<endl;
         return;
     }
     if(level == n || sum > t) return;
     
-    recursion(level + 1, sum);
-    recursion(level + 1, sum + input[level])
+    index++;
+   
+    ans[index] = input[level];
+    //cout<<input[level];
+    recursion(level + 1, sum + input[level], ans, index);
+    ans[index] = 0;
+    recursion(level + 1, sum, ans, index);
 }
 
 int main()
@@ -25,9 +33,12 @@ int main()
        for(int i = 0; i < n; i++){
            cin >> input[i];
        }
+       cout<<"sum of "<<t<<endl;
+       int ans[15]={0};
+    //   int index = 0;
+       recursion(0,0,ans,0);
        
    }
    
    return 0;
 }
-
