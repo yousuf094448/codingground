365c56b231473f22e0688a8f791a7dc7dfab9f9a
yousuf094448 <faishal.yousuf@yahoo.com>
2016-07-01 12:17:19 +0600

    Update 514-Sum it up.cpp

--- 514-Sum it up.cpp
+++ 514-Sum it up.cpp
@@ -1,6 +1,33 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-  cout<<"Hello Yousuf";
-  return 0;
+
+int input[15];
+int t,n;
+
+void recursion(int level, int sum){
+    if(sum == t){
+        cout << "yes" << endl;
+        return;
+    }
+    if(level == n || sum > t) return;
+    
+    recursion(level + 1, sum);
+    recursion(level + 1, sum + input[level])
 }
+
+int main()
+{
+   freopen("input.txt","r",stdin);
+   
+   while(true){
+       cin >> t >> n;
+       if(t == 0) break;
+       for(int i = 0; i < n; i++){
+           cin >> input[i];
+       }
+       
+   }
+   
+   return 0;
+}
+
