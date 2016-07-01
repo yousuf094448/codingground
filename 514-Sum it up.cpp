5b7be28ddba0d974970ab63c7739636b9e512004
yousuf094448 <faishal.yousuf@yahoo.com>
2016-07-01 17:52:00 +0600

    Update 514-Sum it up.cpp

--- 514-Sum it up.cpp
+++ 514-Sum it up.cpp
@@ -4,23 +4,16 @@ using namespace std;
 int input[15];
 int t,n;
 int temp[15]={0};
+bool none = true;
 
 void recursion(int level, int sum, int ans[], int index){
     if(sum == t){
         int flag=0;
         int tem[15];
-        // for( i = 0; i<=n; i++){
-        //     cout<<temp[i]<<" "<<ans[i]<<endl;
-        //     if(temp[i]!=ans[i]) break;
-        // }
-        // if(i==n) return;
         int j=0;
         for(int i = 0; i<=n; i++){
-            //temp[i]=ans[i];
             if(ans[i]){
-                // if(flag)cout<<"+";
                 tem[j++]=ans[i];
-                // flag=true;
             } 
         }
         for(int i = 0; i<j; i++){
@@ -30,24 +23,13 @@ void recursion(int level, int sum, int ans[], int index){
             }
         }
         if(!flag) return;
-        // j=0;
-        flag=0;
         for(int i = 0; i<j; i++){
             if(i)cout<<"+";
                 cout<<tem[i];
                 temp[i]=tem[i];
-                // flag=1;
         }
-        // for(int i = 0; i<=n; i++){
-        //   // temp[i]=ans[i];
-        //     if(ans[i]){
-        //         if(flag)cout<<"+";
-        //         cout<<ans[i];
-        //         temp[j++]=ans[i];
-        //         flag=1;
-        //     } 
-        // }
         cout<<endl;
+        none = false;
         return;
     }
     if(level == n || sum > t) return;
@@ -63,7 +45,7 @@ void recursion(int level, int sum, int ans[], int index){
 
 int main()
 {
-   freopen("input.txt","r",stdin);
+//    freopen("input.txt","r",stdin);
    
    while(true){
        cin >> t >> n;
@@ -71,10 +53,12 @@ int main()
        for(int i = 0; i < n; i++){
            cin >> input[i];
        }
-       cout<<"sum of "<<t<<endl;
+       cout<<"Sums of "<<t<< ":" << endl;
        int ans[15]={0};
+       none = true;
     //   int index = 0;
        recursion(0,0,ans,0);
+       if(none) cout << "NONE" << endl;
        
    }
    
